3d9a6b39fa62dfb7c13cd986b3c1ae5899a9e94e
ajeetydv4545 <ajeetydv1010@gmail.com>
2026-07-13 12:26:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = "Error";
+    }
+}
+
+document.addEventListener("keydown",(e)=>{
+
+    if((e.key >= "0" && e.key <= "9") ||
+       "+-*/().".includes(e.key)){
+        appendValue(e.key);
+    }
+
+    if(e.key === "Enter"){
+        calculate();
+    }
+
+    if(e.key === "Backspace"){
+        deleteLast();
+    }
+
+    if(e.key === "Escape"){
+        clearDisplay();
+    }
+});
